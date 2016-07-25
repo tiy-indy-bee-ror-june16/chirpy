951c7f954f9445a36f3cce219cc0dc8ee9453234
Chris Vannoy <chris.vannoy@theironyard.com>
2016-07-25 13:08:13 -0400

    Adds current_user to users#index serializer

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
 
   def index
     @users = User.where.not(id: current_user.id)
-    render json: @users
+    render json: @users, scope: current_user, scope_name: :current_user
   end
 
   def create
